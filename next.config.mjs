01fc681be556a70cedbb474d8c4b592be3504661
Charles Wang <jhwang@fosterandpartners.com>
2024-11-12 09:58:47 +0000

    demo-topo

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {webpack: (config) => {
+    config.externals = [...config.externals, { canvas: "canvas" }]; // required to make Konva & react-konva work
+    return config;
+  },};
 
 export default nextConfig;
