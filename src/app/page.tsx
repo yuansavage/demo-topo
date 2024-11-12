b5a38d768d8d22071a85617affaefb0fcadc7e2c
Charles Wang <jhwang@fosterandpartners.com>
2024-11-12 10:07:40 +0000

    install shadcn ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,12 @@ import dynamic from "next/dynamic";
 const MapComponent = dynamic(() => import("../../component/Map"), {
   ssr: false,
 });
+import { Button } from "@/components/ui/button";
 
 export default async function Home() {
   return (
     <div className="flex flex-col items-center gap-4">
+      <Button variant="outline">Button</Button>
       <MapComponent />
     </div>
   );
